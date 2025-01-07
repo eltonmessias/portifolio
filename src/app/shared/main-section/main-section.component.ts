7b3834c3a0fe20a00d2c0bab23a1ec1cddd94b0c
elton <eltonguambe1006@gmail.com>
2025-01-07 14:33:05 +0200

    dwl cv

--- src/app/shared/main-section/main-section.component.ts
+++ src/app/shared/main-section/main-section.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainAnimationComponent } from "../main-animation/main-animation.component";
+import { BtnPrimaryComponent } from "../btn-primary/btn-primary.component";
 
 @Component({
   selector: 'app-main-section',
   standalone: true,
-  imports: [MainAnimationComponent],
+  imports: [MainAnimationComponent, BtnPrimaryComponent],
   templateUrl: './main-section.component.html',
   styleUrl: './main-section.component.scss'
 })
